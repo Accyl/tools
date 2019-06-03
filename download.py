1f3338920007ca5d768e6a22e0f640e9aafa1a80
Luna <Luna@cyl-mail.com>
2019-06-03 21:10:46 +0800

    优化代码

--- download.py
+++ download.py
@@ -9,7 +9,7 @@
 
 该工具支持密码与秘钥两种权限认证方式
 
-@author Accyl <email@accyl.cn>
+@author Luna <luna@cyl-mail.com>
 """
 
 import argparse
@@ -53,17 +53,21 @@ def connect_ssh(host: str, port: int = 22, username: str = 'root', passwd: str =
                 ssh.connect(hostname=host, port=port, username=username, password=passwd)
         except paramiko.AuthenticationException as exception:
             logger.error("身份验证失败：{0}".format(exception))
+
             return False
         except paramiko.SSHException as exception:
             logger.error("连接远程服务器失败：{0}".format(exception))
+
             return False
         except ssh_exception.NoValidConnectionsError as exception:
             logger.error("无法连接服务器：{0}".format(exception))
+
             return False
 
         return ssh
     except KeyboardInterrupt:
         logger.info('用户手动结束进程，程序退出')
+
         return False
 
 
@@ -83,14 +87,16 @@ def remote_download_file(ssh: paramiko.SSHClient, link: str, save_name: str) -> 
             stdout.read()
         except paramiko.SSHException as exception:
             logger.error("执行远程命令失败：{0}".format(exception))
+
             return False
 
         logging.info("远程下载完成")
+
         return True
     except KeyboardInterrupt:
         logger.info('用户手动结束进程，执行清理程序')
-        remove_remote_download_file(ssh, save_name)
-        exit(0)
+
+        return False
 
 
 def download_file_to_local(ssh: paramiko.SSHClient, filename: str, target: str, target_filename: str) -> bool:
@@ -109,6 +115,7 @@ def download_file_to_local(ssh: paramiko.SSHClient, filename: str, target: str, 
             sftp = ssh.open_sftp()
         except paramiko.SSHException as exception:
             logging.error("FTP连接失败：{0}".format(exception))
+
             return False
 
         file_path = "/tmp/{0}".format(filename)
@@ -116,6 +123,7 @@ def download_file_to_local(ssh: paramiko.SSHClient, filename: str, target: str, 
             file_stat = sftp.stat(file_path)
         except FileNotFoundError:
             logger.error("SFTP无法找到指定路径的文件：{0}".format(file_path))
+
             return False
 
         target_file_path = os.path.join(target, target_filename)
@@ -126,23 +134,26 @@ def download_file_to_local(ssh: paramiko.SSHClient, filename: str, target: str, 
                 open(target_file_path + shift, 'w').close()
             except PermissionError:
                 logger.error('该目录没有写入权限:{0}'.format(target))
+
                 return False
         else:
             prompt = str(input('已存在同名文件，是否覆盖[y/N]:')).lower()
             if 'n' == prompt[0] if prompt else 'n':
                 logger.info('下载结束，程序退出')
+
                 return False
 
         logger.info("开始回传：{0}".format(target_filename))
 
         bar = progressbar.ProgressBar()
-        bar.start(file_stat.st_size)
+        bar.start(int(file_stat.st_size / 1024))
         try:
-            sftp.get(file_path, target_file_path + shift, lambda size, total: bar.update(size))
+            sftp.get(file_path, target_file_path + shift, lambda size, total: bar.update(int(size / 1024)))
         except IOError as exception:
             sftp.close()
 
             logger.error("回传文件失败：{0}".format(exception))
+
             return False
         except SFTPError as exception:
             bar.finish()
@@ -150,6 +161,7 @@ def download_file_to_local(ssh: paramiko.SSHClient, filename: str, target: str, 
 
             sftp.close()
             os.remove(target_file_path + shift)
+
             return False
 
         bar.finish()
@@ -161,6 +173,7 @@ def download_file_to_local(ssh: paramiko.SSHClient, filename: str, target: str, 
         return True
     except KeyboardInterrupt:
         logger.info('用户手动结束进程，执行清理程序')
+
         return False
 
 
@@ -178,6 +191,7 @@ def remove_remote_download_file(ssh: paramiko.SSHClient, filename: str):
         stdout.read()
     except paramiko.SSHException as exception:
         logger.error("执行远程命令失败：{0}".format(exception))
+
         return False
 
     return True
@@ -187,7 +201,7 @@ if '__main__' == __name__:
     default_ssh_key = os.path.join(os.environ['HOME'] if sys.platform in ['darwin', 'linux'] else '', '.ssh', 'id_rsa.pub')
     parser = argparse.ArgumentParser(description="通过远程下载服务器进行文件下载，再通过FTP将文件下载到本地，适用于墙外软件的下载")
     parser.add_argument('host', help="远程服务器地址")
-    parser.add_argument('file', help='下载连接地址')
+    parser.add_argument('file', help='要下载的文件地址')
     parser.add_argument('-t', '--target', help="保存位置，默认为当前目录", default='.')
     parser.add_argument('-p', '--port', help="远程服务器端口", default=22)
     parser.add_argument('-u', '--username', help="登录远程服务器的用户名", default="root")
@@ -208,10 +222,10 @@ if '__main__' == __name__:
         file_name = os.path.split(parser_args.path)[-1]
         suffix = "*###*{0}{1}".format(datetime.datetime.now(), random.random())
 
-        remote_download_file(ssh_handle, args.file, "{0}{1}".format(file_name, suffix))
+        remote_save_name = "{0}{1}".format(file_name, suffix)
+        if remote_download_file(ssh_handle, args.file, remote_save_name) is True:
+            download_file_to_local(ssh_handle, remote_save_name, args.target, file_name)
 
-        download_file_to_local(ssh_handle, "{0}{1}".format(file_name, suffix), args.target, file_name)
-
-        remove_remote_download_file(ssh_handle, "{0}{1}".format(file_name, suffix))
+        remove_remote_download_file(ssh_handle, remote_save_name)
 
         ssh_handle.close()
